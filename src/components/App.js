5b705b69e66a6fdb49663e8ff2fc9021cfd9b300
Caleb Frost Sanderson <calebsanderson@gmail.com>
2016-12-11 18:08:33 -0500

    added comments for what is happening in the BoxItems.js component and started working on the App.js component. Will complete App.js notes and then attempt to rewrite

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,36 @@
+/* bring in the magic Component object from react and set it to the keyword React */
 import React, { Component } from 'react'
+/* bring in the related stylesheet by associating it with an scss file */
 import styles from './App.scss'
+/* bring in the stylesheet from any other scss files that have to do with elements represented here */
 import BoxItem from './BoxItem'
 
+// bring in any images or files stored in a json file
 import images from '../images.json'
 
+// start this component function
 class App extends Component {
 
+  // set the initial state of the app using this function
   constructor () {
+    // set the hierarchy of this initial state to it's superior
     super()
+    // explicitly say what the initial state of the app should be with this initial function
     this.state = {
+      // use keys and set their initial values (3x)
       matched: [],
       picks: [],
       won: false
     }
   }
-
+  // this function sets what happens when the cards are chosen
   choose (card) {
-    if (this.state.picks.length === 2 || this.state.matched.includes(card)) { return } // DO NOTHING, BAIL OUT
+    /* an if statement that says "if the 'picks length state' is 2
+    OR the 'matched state' includes the original card
+    then return/bail out of the function" */
+    if (this.state.picks.length === 2 || this.state.matched.includes(card)) { return }
+    /* at this point, if the above if test failed,
+    then set the state to what is in the current picks array */
     this.setState({
       picks: [...this.state.picks, card]
     }, () => {
@@ -34,7 +48,6 @@ class App extends Component {
         picks: []
       }, () => {
         if (this.state.matched.length === images.length) {
-          // Show modal
           this.setState({
             won: true
           })
@@ -46,14 +59,31 @@ class App extends Component {
     }, 2000)
   }
 
+  // this is the basic function for every React component.
   render () {
+    // set constants for primary states of the actionable items
     const picks = this.state.picks
     const matched = this.state.matched
+    // start laying out 'returning' the basic JSX for the page
     return <div>
+      {/* - this section must be wrapped in a div.
+          - elements can be set with inline functions that determine their state dependant on conditions (if/else) */}
+      {/* a title or message ba */}
       <h1>{this.state.won ? 'YOU WIN!!!' : 'Bill MEMEurray'}</h1>
+      {/* the basic gameboard */}
       <section>
+        {/* a table */}
         <table>
+          {/* table rows */}
           <tr>
+            {/* The cards go here which will appear as components inside self-closing JSX angle brackets.
+            These items are individual expressions of the components set up in the other component file. */}
+            {/* Each item/element/component-expression contains...
+              - the name of the component which acts as a self-closing JSX element
+              - image class that is set to a function that calls a position in the array of images contained in the referenced
+                .json file where the images are located.
+              -
+              - */}
             <BoxItem image={images[0]} isUp={picks.includes(0) || matched.includes(0)} handleClick={() => this.choose(0)} />
             <BoxItem image={images[1]} isUp={picks.includes(1) || matched.includes(1)} handleClick={() => this.choose(1)} />
             <BoxItem image={images[2]} isUp={picks.includes(2) || matched.includes(2)} handleClick={() => this.choose(2)} />
@@ -83,4 +113,5 @@ class App extends Component {
   }
 }
 
+// export this component
 export default App
--- src/components/BoxItem.js
+++ src/components/BoxItem.js
@@ -1,17 +1,38 @@
+/* import the built-in component function from React */
 import React, { Component } from 'react'
+/* import styles from the applicable stylesheet folders. Lines __ and __ point to these styles */
 import styles from './BoxItem.scss'
 
+/* set up this component using the "class ComponentName extends Component" syntax */
 class BoxItem extends Component {
 
+  /* a basic render function required by all React components that will manage each card's actions */
   render () {
+    /* Make a 'potato' constant called upClass that is set to an inline if/else statement
+    (using a Conditional (ternary) Operator = https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator)
+    The statement should check the 'isUp' property using this.props.isUp (which should evaluate to either true/false).
+    The card's 'isUp' property is set to false by default. If the card has beeen clicked then it will evaluate to true
+    and therefore will change the style to 'up'. */
     const upClass = this.props.isUp ? styles.up : styles.down
+    /* 'return' a JSX table data cell with onClick and className properties */
     return <td
+      /* the onClick property is set to call a function that takes as it's arguement the 'handleClick' function that is
+      on each element/component in the App.js render section (e.g. last property on any one of the <BoxItem /> components) */
       onClick={() => this.props.handleClick()}
+      /* set the className to a string that is an array made up of 'styles.root' and 'upClass' joined by a space between them */
       className={[styles.root, upClass].join(' ')}>
+      {/* add a self-closing img tag here with it's src set to an string of 'this.props.image' which points to the image
+      url given to it in App.js component in the App.js render section which would in turn set it to the given postiion in the array of images
+      in the images.json file. */}
       <img src={this.props.image} />
+    {/* close the element */}
     </td>
   }
-
 }
 
+/* export the component using the "export default ComponentName" syntax. This should be the last thing in the file. */
 export default BoxItem
+
+/* Some questions, terms to define, and/or ways to talk about what is happening...
+- is this a "Potato"?
+-
